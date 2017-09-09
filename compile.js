c8677830a3ceab4f0efa5dac5a6fc68261153da7
chris rollins <chris.rollins@leapfire.com>
2017-09-09 11:08:08 -0700

    now console logs whether the library initialized successfully. compile.js now outputs time taken for each step (read, compile, additional modifications, write)

--- compile.js
+++ compile.js
@@ -1,8 +1,34 @@
+function timeAction(action)
+{
+	const start = process.hrtime();
+	action();
+	const diff = process.hrtime(start);
+	return `${diff[0] + diff[1] / 1e9}s`;
+}
+
+console.log("Loading dependencies...");
+
 const fs = require("fs");
 const babel = require("babel-core");
 
-const result = babel.transformFileSync("./src/crystalline.js", {comments: false, minified: false, sourceType: "module", compact: false, presets: ["es2015"]});
+console.log("Compiling...");
 
-let code = `var Crystalline = (function(){\n${(result.code).replace("var Crystalline = ", "return ")} })();`;
+let source, result, code;
 
-fs.writeFileSync("crystalline.js", code, {encoding: "utf-8"});
+console.log("Reading source file: ".padEnd(30) + timeAction(function(){
+	source = fs.readFileSync("./src/crystalline.js");
+}) );
+
+console.log("Compiling: ".padEnd(30) + timeAction(function(){
+	result = babel.transform(source, {comments: false, minified: false, sourceType: "module", compact: false, presets: ["es2015"]});
+}) );
+
+console.log("Additional modifications: ".padEnd(30) + timeAction(function(){
+	code = `var Crystalline = (function(){\n${(result.code).replace("var Crystalline = ", "return ")} })();`;
+}) );
+
+console.log("Writing output file: ".padEnd(30) + timeAction(function(){
+	fs.writeFileSync("crystalline.js", code, {encoding: "utf-8"});
+}) );
+
+console.log("Done.");
